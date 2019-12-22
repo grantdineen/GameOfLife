337bf16ed2a338d522e3ca04d5d2c80fe8c7370b
Grant Dineen <grantdineen10@hotmail.com>
2019-12-22 14:15:19 -0500

    Added basic game of life functionality

--- js/gameoflife.js
+++ js/gameoflife.js
@@ -1,13 +1,32 @@
 
 const HEIGHT = 800;
 const WIDTH = 800;
-const SQUARES_PER_ROW = 30;
-const SQUARES_PER_COL = 30;
+const SQUARES_PER_ROW = 50;
+const SQUARES_PER_COL = 50;
 
-let board = [];
-
+let playButton = document.getElementById("playButton");
 let canvas = document.getElementById("canvas");
 let ctx = canvas.getContext("2d");
+let board = [];
+let isPlaying = false;
+
+
+function initializeArray(array) {
+    array = [SQUARES_PER_COL];
+    for (let i = 0; i < SQUARES_PER_COL; i++) {
+        array[i] = [];
+        for (let j = 0; j < SQUARES_PER_ROW; j++) {
+            array[i][j] = 0;
+        }
+    }
+    return array;
+}
+
+//play button listener
+playButton.addEventListener('click', function (event) {
+    playButton.textContent = isPlaying ? "Play" : "Stop";
+    isPlaying = !isPlaying;
+}, false);
 
 //on mouse click listener
 canvas.addEventListener('click', function (event) {
@@ -19,17 +38,8 @@ canvas.addEventListener('click', function (event) {
     let rowIndex = Math.floor(x / WIDTH * SQUARES_PER_ROW);
     board[colIndex][rowIndex] = 1;
     drawBoard();
-}, false);
 
-function initializeArray() {
-    board = [SQUARES_PER_COL];
-    for (let i = 0; i < SQUARES_PER_COL; i++) {
-        board[i] = [];
-        for (let j = 0; j < SQUARES_PER_ROW; j++) {
-            board[i][j] = 0;
-        }
-    }
-}
+}, false);
 
 function drawBoard() {
     for (let y = 0; y < HEIGHT; y += HEIGHT / SQUARES_PER_COL) {
@@ -53,5 +63,55 @@ function drawBoard() {
     }
 }
 
-initializeArray();
+function update() {
+    let tempArray;
+    tempArray = initializeArray(tempArray);
+
+    for (let i = 0; i < board.length; i++) {
+        for (let j = 0; j < board[i].length; j++) {
+            let numOfLiveCells = getNumberOfSurroundingLiveCells(board, i, j);
+
+            if (board[i][j] === 0 && numOfLiveCells === 3) // create new living cells
+                tempArray[i][j] = 1;
+            else if (board[i][j] === 1 && (numOfLiveCells < 2 || numOfLiveCells > 3)) //kill over/under populated cells
+                tempArray[i][j] = 0;
+            else
+                tempArray[i][j] = board[i][j];
+        }
+    }
+
+    board = tempArray;
+}
+
+function getNumberOfSurroundingLiveCells(array, col, row) {
+    let numOfLiveCells = 0;
+
+    //get boundaries
+    let colIndex = Math.max(0, col - 1);
+    let colLength = Math.min(array.length - 1, col + 1);
+    let rowIndex = Math.max(0, row - 1);
+    let rowLength = Math.min(array[0].length - 1, row + 1);
+
+    for (let i = colIndex; i <= colLength; i++) {
+        for (let j = rowIndex; j <= rowLength; j++) {
+            if (i == col && j === row)
+                continue;
+
+            if (array[i][j] === 1)
+                numOfLiveCells++;
+        }
+    }
+
+    return numOfLiveCells;
+}
+
+function gameLoop() {
+    if (!isPlaying)
+        return;
+    update();
+    drawBoard();
+}
+
+board = initializeArray(board);
 drawBoard();
+window.setInterval(gameLoop, 250);
